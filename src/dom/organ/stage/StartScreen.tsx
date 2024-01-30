a2cacab7931ca9b04ff689e5bb8dcdaedab8164e
Abraham Duno <abraham.altmail@gmail.com>
2024-01-30 12:47:04 -0400

    sfx 3

--- src/dom/organ/stage/StartScreen.tsx
+++ src/dom/organ/stage/StartScreen.tsx
@@ -23,14 +23,16 @@ export const StartScreen = ({ state, calls, showStart=true }: any) => {
             >
                 Start
             </div>
-            <div className="z-300 pos-abs bottom-0 right-0 px-4 pos-rel tx-lx tx-bold-6 opaci-chov--50 tx-altfont-1 tx-white tx-shadow-5 hover-3 mt-8"
+            <div className="z-300 pos-abs bottom-0 right-0 px-4 pos-rel flex-col tx-bold-6 opaci-chov--50 tx-altfont-1 tx-white tx-shadow-5  mt-8"
         style={{textShadow: "2px 2px 0 #000000, 0 15px 20px #00336677"}}
         onClick={() => {
                     if (state.isPlaying) { return; }
                     calls.s__isPlaying(true);
                 }}
             >
-                Continue
+                <div className="Q_sm_x tx-lx"> Continue </div>
+                <div className="Q_xs tx-lgx"> Continue </div>
+                <div className="tx-lg"> Level 1 </div>
             </div>
             </>}
             <div className="pos-abs top-0 left-0 flex-col "
@@ -89,13 +91,15 @@ const adjustBgVolume = (volume: number) => {
     
     
         
-    <div className="pos-abs top-50p  mt-6 tx-shadow-5 bord-r-100 bg-w-50 border-white right-25p hover-6 z-200 " onClick={
+    <div className="pos-abs   mt-6 tx-shadow-5 bord-r-100 bg-w-50 border-white hover-jump z-200  audioIcon"
+    
+    onClick={
         ()=>{
             
         }}>
             
-            <details className='pos-rel'>
-                <summary className='flex opaci-chov--50 tx-xl '
+            <details className='pos-rel z-200'>
+                <summary className='flex opaci-chov--50 tx-xl  '
                 onClick={()=>{
                     if (!currentSpeech && !currentSpeechBg) {
                         audioNotification("../sound/speech.mp3", )
@@ -104,7 +108,7 @@ const adjustBgVolume = (volume: number) => {
                 }}
                 ><div>🔊</div></summary>
                 <div className='pos-abs right-0 mt-2'>
-                    <div className='flex-col gap-2 flex-align-end'>
+                    <div className='flex-col gap-2 flex-align-end bg-w-10 bord-r-25 border-white-50 bg-glass-10 pa-1 py-2'>
                     
                         <button className='nowrap opaci-chov--50 py-2  px-3 bg-b-50 bord-r-10 tx-white border-white ' onClick={()=>{
                             audioNotification("../sound/speech.mp3", )}}>
